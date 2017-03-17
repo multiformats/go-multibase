8cb3334d95dcd15685fa2a317ae7996b5f87273f
Jakub Sztandera <kubuxu@protonmail.ch>
2017-03-17 15:39:31 +0100

    Add streaming to multibase-conv

--- multibase-conv/main.go
+++ multibase-conv/main.go
@@ -3,30 +3,32 @@ package main
 import (
 	"fmt"
 	"os"
-	
+
 	multibase "github.com/multiformats/go-multibase"
 )
 
 func main() {
-	if len(os.Args) != 3 {
-		fmt.Printf("usage: %s CID NEW-BASE\n", os.Args[0])
+	if len(os.Args) < 3 {
+		fmt.Printf("usage: %s NEW-BASE CID...\n", os.Args[0])
 		os.Exit(1)
 	}
 
-	cid := os.Args[1]
-	newBase := os.Args[2]
+	newBase := os.Args[1]
+	cids := os.Args[2:]
 
-	_, data, err := multibase.Decode(cid)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
-		os.Exit(1)
+	for _, cid := range cids {
+		_, data, err := multibase.Decode(cid)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			os.Exit(1)
+		}
+
+		newCid, err := multibase.Encode(multibase.Encoding(newBase[0]), data)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+			os.Exit(1)
+		}
+		fmt.Println(newCid)
 	}
 
-	newCid, err := multibase.Encode(multibase.Encoding(newBase[0]), data)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("%s\n", newCid)
 }
